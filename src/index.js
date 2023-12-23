f58e19c2d10167f46c31338fc703126758006500
Andrii Hutnyk <andreigutnik@gmail.com>
2023-12-23 09:07:35 +0100

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter basename="WaterTracker_Frontend">
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
