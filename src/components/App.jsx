cb56b34cc2213fe3eb894350d2748e759692daed
Andrii Hutnyk <andreigutnik@gmail.com>
2023-12-24 20:23:17 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { GlobalStyle } from './GlobalStyle';
 import { Container } from './Container';
-import Loader from './Loader/Loader';
+// import Loader from './Loader/Loader';
 // import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
