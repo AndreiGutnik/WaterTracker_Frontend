d1dba7e49643d79f4667dc5eb65e2f89faec4a8e
MiraDov290 <muzhyk.myroslava@gmail.com>
2023-12-26 10:14:19 +0000

    changer

--- src/components/Setting/Setting.jsx
+++ src/components/Setting/Setting.jsx
@@ -2,12 +2,19 @@ const Setting = ({ children, closeModal }) => {
     return (
         <div
             className='modal fade show'
-            style={{ display: 'block', backdropFilter: 'blur(5px)' }}
+            // style={{ display: 'block', backdropFilter: 'blur(5px)' }}
         >
             <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h2 className="modal-title">Setting</h2>
+                        <h3 className="modal-title"
+        //                     style={{
+                            
+        // margin: 8,
+        // padding: "12px 16px",
+        // color: "black",
+                        //   }}
+                        >Setting</h3>
                         <button
                             type="button"
                             className="bth-close"
