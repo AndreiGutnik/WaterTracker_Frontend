c3008938a4ceca9c1482a5d927049c319eacf59f
Pavlo Lysiuk <121764119+PavloLysiuk@users.noreply.github.com>
2024-01-03 18:25:24 +0200

    edit styles

--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -11,11 +11,15 @@ export const TrackerContainer = styled.div`
   background: ${baseTheme.colors.grey};
   box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.3);
 
+  outline: 1px solid green;
+
   @media screen and (min-width: 768px) {
     padding: 32px 24px;
+    width: 704px;
+    height: 688px;
   }
   @media only screen and (min-width: 1440px) {
     width: 592px;
-    height: 690px;
+    height: 680px;
   }
 `;
