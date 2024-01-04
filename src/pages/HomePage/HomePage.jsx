9939770caa7c9f7c6944ce58f9806f6a75b0ccb5
MiraDov290 <muzhyk.myroslava@gmail.com>
2024-01-04 19:03:35 +0000

    chang

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,13 @@
 import { Calendar } from 'components/Calendar/Month/Month';
 import { TodayWaterList } from 'components/TodayWaterList/TodayWaterList';
 import { TrackerContainer } from './HomePage.styled';
+import HeaderSetting from 'components/Setting/HeaderSetting';
+import SettingModal from 'components/Setting/SettingModal';
 
 const HomePage = () => {
   return (
     <TrackerContainer>
+      <SettingModal/>
       <TodayWaterList />
       <Calendar />
     </TrackerContainer>
