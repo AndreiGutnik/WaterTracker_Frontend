164439e0929e8b555a31ed39685bd89179ba1a17
MiraDov290 <muzhyk.myroslava@gmail.com>
2024-01-04 09:22:41 +0000

    chang

--- src/components/Setting/HeaderSetting.jsx
+++ src/components/Setting/HeaderSetting.jsx
@@ -1,9 +1,9 @@
 const HeaderSetting = ({ showModal }) => {
-
+    
     return (
-            <div className="container-fuild ">
-                <button className="btn btn-outline-success" onClick={showModal}>Setting</button>
-            </div>
+            <div className="container-fuild">
+        <button className="btn btn-outline-success" onClick={showModal}>Setting</button>
+            </div> 
     )
 }
 export default HeaderSetting
