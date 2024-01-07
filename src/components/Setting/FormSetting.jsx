ac7848c36241d2ab02313dc3c03d8dd359ed1c77
MiraDov290 <muzhyk.myroslava@gmail.com>
2024-01-07 07:48:56 +0000

    chang

--- src/components/Setting/FormSetting.jsx
+++ src/components/Setting/FormSetting.jsx
@@ -12,7 +12,6 @@ import {
     FormCheck,
     LabelName,
     GenderRadio,
-    SpanFormChechHender,
     Formy,
     FormText
 } from './FormSetting.styled';
@@ -21,27 +20,27 @@ import {
 
  
 export const FormSetting = () => {
-    const [errorMessage, setErrorMessage] = useState('');
     const { user } = useAuth();
-    const [name = user.name, setName ]= useState();
-    const [gender, setGender] = useState();
-    const [email, setEmail ]= useState();
-    const [password, setPassword] = useState(null);
-    const [newPassword = '', setNewPassword] = useState(null);
+    const [name = user.name, setName ]=useState();
+    const [gender = user.gender, setGender] = useState();
+    const [email = user.email, setEmail ]= useState();
+    const [password = user.password, setPassword] = useState();
+    const [newPassword = '', setNewPassword] = useState();
     const [repeatPassword = '', setRepeatPassword] = useState();
+    const { showPassword } = useState();
     const dispatch  = useDispatch()
     let isSubmit = true;
     
     const state = {
-        // photo: '',
+        photo: '',
         gender: gender,
         name:name,
         email: email,
         password: password,
         newPassword: newPassword,
-        
-        // onClick: 'false',
-        // showPassword: 'false',
+        repeatPassword: repeatPassword,
+        onClick: 'false',
+        showPassword: 'false',
     }
 
     const handleChangePassword = (e) => {    
@@ -70,29 +69,20 @@ export const FormSetting = () => {
     
     const handleSubmit = (e) => {
         e.preventDefault()
-        const{newPassword,repeatPassword}= e.target.value
-        // updateUserDataSubmit(e);
+        updateUserDataSubmit(e);
+       
+    }
+
+    const updateUserDataSubmit = (e) =>{
+        console.log(state);
+        console.log(isSubmit);
         isSubmit = false;
-        if (newPassword !== repeatPassword) {
-        return
-        }
-        setNewPassword(newPassword)
-        dispatch(updateUserData(state));
+
+
+
+      dispatch(updateUserData(state));
     }
 
-    // const updateUserDataSubmit = (e) =>{
-    //     console.log(state);
-    //     console.log(isSubmit);
-    //     isSubmit = false;
-
-
-
-    //   dispatch(updateUserData(state));
-    // }
-
-    const handleVerificationUserPassword = (e) => {
-        state.password = e.target.value;
-    }
 
 
     return (
@@ -121,30 +111,30 @@ export const FormSetting = () => {
                     <LabelName>Password</LabelName>
                     <FormText>Outdated password:</FormText>
                     <div className="passwordClass">
-                        <input name="password" type={state.showPassword ? "text" : "password"}
+                        <input name="password" type={showPassword ? "text" : "password"}
                             className="form-control pass" id="exampleInputPassword1"
                             onChange={handleChangeOldPassword}
                             value={password}
                             placeholder="Password"
-                            />
+                            required/>
                     </div>
                 </Formy>       
             <Formy>
                 <FormText>New Password:</FormText>
                 <div className="passwordClass">
-                    {/* <i className="show-button" onClick={() => setPassword({ showPassword: !state.showPassword })}>
+                    <i className="show-button" onClick={() => setPassword({ showPassword: !state.showPassword })}>
                         {state.showPassword ?
                             <svg width="16" height="16" fill="blue" aria-label="upload picture" component="span">
                                 <use href={Icons + '#eye-show'}></use></svg> :
                             <svg width="16" height="16" fill="blue" aria-label="upload picture" component="span">
                                 <use href={Icons + '#eye-hide'}></use></svg>}
-                    </i> */}
+                    </i>
                     <input name="newPassword" type={state.showPassword ? "text" : "password"}
                         className="form-control pass" id="exampleInputPassword2"
-                        // onChange={handleChangeNewPassword}
+                        onChange={handleChangeNewPassword}
                         value={newPassword}
                         placeholder="Password"
-                        
+                        required
                     />       
                 </div>
             </Formy>
@@ -161,10 +151,10 @@ export const FormSetting = () => {
                     </i> */}
                     <input name="repeatPassword" type={state.showPassword ? "text" : "password"}
                         className="form-control" id="exampleInputPassword3"
-                        // onChange={handleChangePassword}
+                        onChange={handleChangePassword}
                         value={repeatPassword}
                         placeholder="Password"
-                       
+                        required
                     />
                 </div>
             </Formy>
