123db866bfddec74e5e74193bcbc8f545eca2d14
Andrii Hutnyk <andreigutnik@gmail.com>
2024-01-08 09:09:58 +0100

    Update userModal

--- src/components/Header/ModalHeader/ModalHeader.jsx
+++ src/components/Header/ModalHeader/ModalHeader.jsx
@@ -1,35 +1,20 @@
-// import { useEffect } from 'react';
-// import Modal from 'react-modal';
-
+import { useEffect } from 'react';
 import { ModalWrapper } from './ModalHeader.styled';
 
-// const customStyles = {
-//   content: {
-//     top: '50%',
-//     left: '50%',
-//     right: 'auto',
-//     bottom: 'auto',
-//     marginRight: '-50%',
-//     transform: 'translate(-50%, -50%)',
-//   },
-// };
-
-// Modal.setAppElement('#modal-root');
-
 export const ModalHeader = ({ isOpen, onClose, children }) => {
-  // useEffect(() => {
-  //   const close = e => {
-  //     if (e.key === 'Escape') {
-  //       onClose();
-  //     }
-  //   };
-  //   document.addEventListener('keydown', close);
-  //   document.addEventListener('mousedown', close);
-  //   return () => {
-  //     document.removeEventListener('keydown', close);
-  //     document.removeEventListener('mousedown', close);
-  //   };
-  // }, [onClose]);
+  useEffect(() => {
+    const close = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    document.addEventListener('keydown', close);
+    document.addEventListener('mousedown', onClose);
+    return () => {
+      document.removeEventListener('keydown', close);
+      document.removeEventListener('mousedown', onClose);
+    };
+  }, []);
 
   return <>{isOpen && <ModalWrapper>{children}</ModalWrapper>}</>;
 };
--- src/components/Header/UserLogo/UserLogo.js
+++ src/components/Header/UserLogo/UserLogo.js
@@ -1,27 +1,49 @@
 import { useAuth } from 'hooks/useAuth';
 import Icons from '../../../images/sprite.svg';
-import { ThumbImg, WrapperBtn } from './UserLogo.styled';
+import { Button, ThumbImg, WrapperBtn } from './UserLogo.styled';
 import { useState } from 'react';
 import { ModalHeader } from '../ModalHeader/ModalHeader';
 
 export const UserLogo = () => {
   const { name, avatarURL } = useAuth().user;
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isOpenSettingModal, setIsOpenSettingModal] = useState(false);
+  const [isOpenLogoutModal, setIsOpenLogoutModal] = useState(false);
 
-  const toggleModal = () => setModalIsOpen(!modalIsOpen);
+  const handleOpenModal = setState => setState(true);
+  const handleCloseModal = setState => setModalIsOpen(false);
 
   return (
     <>
-      <WrapperBtn type="button" onClick={toggleModal}>
+      <WrapperBtn type="button" onClick={() => handleOpenModal(setModalIsOpen)}>
         <p>{name}</p>
         <ThumbImg src={avatarURL} alt="logoUser" />
         <svg width="16" height="16" fill="#407BFF">
           <use href={Icons + '#arrow-down'}></use>
         </svg>
       </WrapperBtn>
-      <ModalHeader isOpen={modalIsOpen} onClose={toggleModal}>
-        <p>Settingcccccccccccccccc</p>
-        <p>Log out</p>
+      <ModalHeader
+        isOpen={modalIsOpen}
+        onClose={() => handleCloseModal(setModalIsOpen)}
+      >
+        <Button
+          type="button"
+          onClick={() => handleOpenModal(setIsOpenSettingModal)}
+        >
+          <svg width="16" height="16">
+            <use href={Icons + '#settings'}></use>
+          </svg>
+          <p>Setting</p>
+        </Button>
+        <Button
+          type="button"
+          onClick={() => handleOpenModal(setIsOpenLogoutModal)}
+        >
+          <svg width="16" height="16">
+            <use href={Icons + '#logout'}></use>
+          </svg>
+          <p>Log out</p>
+        </Button>
       </ModalHeader>
     </>
   );
